f3b7ca464b8ea737af4a598e25b7a7e3e99ebb31
dDeeDev <dee.hamdee.084@gmail.com>
2024-04-02 12:13:41 +0700

    :arrow_down: downgrade dependensy

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./token/ERC721/ERC721.sol";
+import "./access/Ownable.sol";
 
 contract myNFT is ERC721, Ownable {
     constructor() ERC721("myNFT", "NFT") {}
